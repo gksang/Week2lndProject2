09ce56c6fcad346ae8bdf7683a51b9cfd384a070
Gilbert Sang <sangkipgil@gmail.com>
2022-01-02 15:24:13 +0300

    Form Variable Update & Conversion

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function akanGen(){
     var name = document.getElementById("name").value;
     var birthDate = document.getElementById("birthDate").value;
     var gender = document.getElementById("gender").value;
-    
+
+    //Extracted Variable conversion
+    var dateString = birthDate.split("/");
+    var dayVal = parseInt(dateString[0]);
+    var monthVal = parseInt(dateString[1]);
+    var yearVal= parseInt(dateString[2]);
+    var yearString = yearVal.toString();
+    var ccVal = parseInt(yearString.substring(0,2));
+    var yyVal = parseInt(yearString.substring(2,4));
+    console.log(dateString);
+
 
 }
