032b5798c7e5046baf3915c35398bf2625313a03
Gilbert Sang <sangkipgil@gmail.com>
2022-01-02 15:05:20 +0300

    Initial Variable declaration and Main Page Layout

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function akanGen(){
+    //Variables to extract values from Forms
+    var name = document.getElementById("name").value;
+    var birthDate = document.getElementById("birthDate").value;
+    var gender = document.getElementById("gender").value;
+    
+
+}
